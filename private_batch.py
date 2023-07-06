3cd3fc0a032ab77b736cfbc4ce0055d9ee9c5463
Hilton Fernandes <fernahdesh@yahoo.com>
2023-07-06 19:42:12 -0300

    2023-07-06 Mid work: Adapted text a little, since answers are not what is expected
    Will use verbose

--- private_batch.py
+++ private_batch.py
@@ -29,7 +29,33 @@ from constants import CHROMA_SETTINGS
 def field_count(obj : object) ->dict:
     return len(obj.__dict__)
 
+def time_at_length(dt : datetime.timedelta) -> str:
+    result : str = ''
+    secs   : int = dt.seconds 
     
+    hours : int = secs // 3600
+    if hours != 0:
+        result += str(hours) + ' h '
+        
+    secs = secs % 3600
+    minutes : int = secs // 60
+    if minutes != 0: 
+        result += str(minutes) + ' min '
+        
+    seconds : int = secs % 60
+    micro   : int = int(round(dt.microseconds / 10000, 0))
+    
+    result = str(seconds)
+        
+    if micro != 0:
+        result += '.' + str(micro)
+    
+    result += ' s'
+    
+    # Normal function termination
+    return result
+
+
 def setup(params : types.SimpleNamespace()) -> RetrievalQA:
     result : RetrievalQA() = None
     
@@ -87,87 +113,61 @@ def setup(params : types.SimpleNamespace()) -> RetrievalQA:
     return result
 
 
-def main():
-
-    # load_dotenv()
-    
-    # embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-    # persist_directory = os.environ.get('PERSIST_DIRECTORY')
-    
-    # model_type = os.environ.get('MODEL_TYPE')
-    # model_path = os.environ.get('MODEL_PATH')
-    # model_n_ctx = os.environ.get('MODEL_N_CTX')
-    # model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
-    # target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
-
+def main() -> int:
     # Parse the command line arguments
     args = parse_cli()
     print(args)
     
-    # embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    # db = Chroma(persist_directory=persist_directory, 
-    #             embedding_function=embeddings, 
-    #             client_settings=CHROMA_SETTINGS)
-    # retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # # activate/deactivate the streaming StdOut callback for LLMs
-    # callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    
-    # # Prepare the LLM
-    # match model_type:
-    #     case "LlamaCpp":
-    #         llm = LlamaCpp(model_path=model_path, 
-    #                        n_ctx=model_n_ctx, 
-    #                        n_batch=model_n_batch, 
-    #                        callbacks=callbacks, 
-    #                        verbose=False)
-    #     case "GPT4All":
-    #         llm = GPT4All(model=model_path, 
-    #                       n_ctx=model_n_ctx, 
-    #                       backend='gptj', 
-    #                       n_batch=model_n_batch, 
-    #                       callbacks=callbacks, 
-    #                       verbose=False)
-    #     case _:
-    #         # raise exception if model_type is not supported
-    #         msg = f"Model type {model_type} is not supported. " +\
-    #             "Please choose one of the following: LlamaCpp, GPT4All"
-    #         raise Exception(msg)
+    if field_count(args) == 0:
+        print('*** The program will be terminated. ***')
         
-    # qa = RetrievalQA.from_chain_type(llm=llm, 
-    #                                  chain_type="stuff", 
-    #                                  retriever=retriever, 
-    #                                  return_source_documents= not args.hide_source)
+        # Return to indicate failure
+        return 1
     
-    qa = setup(args)
-    print(type(qa))
+    params = interp_args(args)    
+    if field_count(params) == 0:
+        print('*** The program will be terminated. ***')
+        
+        # Return to indicate failure
+        return 2
+
+    dt_start = datetime.datetime.now()
+    s_dt_start = dt_start.strftime('%Y-%m-%d %H:%M:%S')
+    print(f'{s_dt_start} Started setup')
+    qa = setup(params)
+    dt_end = datetime.datetime.now()
+    s_dt_end = dt_end.strftime('%Y-%m-%d %H:%M:%S')
+    s_delta = time_at_length(dt_end - dt_start)
+    print(f"\n> {s_dt_end} Finished setup (took {s_delta}):")
     
     # Question and answer
 
-    query = args.single_query
+    query = params.single_query
     
-    # Get the answer from the chain
-    start = time.time()
-
     # Print the query
-    dt_start = datetime.datetime.fromtimestamp(start)
+    dt_start = datetime.datetime.now()
     s_dt_start = dt_start.strftime('%Y-%m-%d %H:%M:%S')
     print(f'\n\n> {s_dt_start} Question: ')
     print(query)
+    
+    # Get the answer from the chain
     res = qa(query)
     answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-    end = time.time()
 
     # Print the result
-    end = time.time()
-    dt_end = datetime.datetime.fromtimestamp(end)
+    dt_end = datetime.datetime.now()
     s_dt_end = dt_end.strftime('%Y-%m-%d %H:%M:%S')
-    print(f"\n> {s_dt_end} Answer (took {round(end - start, 2)} s.):")
+    s_delta = time_at_length(dt_end - dt_start)
+    print(f"\n> {s_dt_end} Answer (took {s_delta}):")
     print(answer)
 
     # Print the relevant sources used for the answer
     for document in docs:
         print("\n> " + document.metadata["source"] + ":")
         print(document.page_content)
+        
+    # Normal function termination
+    return 0
 
 
 def parse_cli() -> argparse.Namespace:
